ec0e15adba2f7c8f1a2e29f1d1bf0c361e8ae164
Gonzalo Reyero Ferreras <greyerof@redhat.com>
2024-04-18 01:48:55 -0500

    Adjust skipped tcs num also.

--- scripts/ci/smoke_test.sh
+++ scripts/ci/smoke_test.sh
@@ -63,7 +63,7 @@ export EXPECTED_VERDICT=${EXPECTED_VERDICT:-"pass"}
 export EXPECTED_TOTAL_TCS=${EXPECTED_TOTAL_TCS:-"92"}
 export EXPECTED_FAILED=${EXPECTED_FAILED:-"0"}
 export EXPECTED_PASSED=${EXPECTED_PASSED:-"4"}
-export EXPECTED_SKIPPED=${EXPECTED_SKIPPED:-"84"}
+export EXPECTED_SKIPPED=${EXPECTED_SKIPPED:-"88"}
 
 # Check the verdit is pass
 echo "$crJson" | jq 'if .status.report.verdict == env.EXPECTED_VERDICT then "verdict is "+env.EXPECTED_VERDICT else error("verdict mismatch: \(.status.report.verdict), expected "+env.EXPECTED_VERDICT) end'
