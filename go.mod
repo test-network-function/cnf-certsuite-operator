be100b5249dea5f37ebab6935575715a5db4300d
Shir Moran <101132224+shirmoran@users.noreply.github.com>
2024-04-30 16:55:44 +0300

    Unit tests using fake client (#52)
    Created UT for the following controller's function  and methods:
    
    - getJobRunTimeThreshold
    - getCertSuiteContainerExitStatus
    - waitForCertSuitePodToComplete
    - updateStatus
    
    Also separated unit tests file generated by GINKGO and unit tests I
    created using a fake client.

--- go.mod
+++ go.mod
@@ -14,6 +14,12 @@ require (
 )
 
 require (
+	github.com/evanphx/json-patch v5.6.0+incompatible // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+)
+
+require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -37,7 +43,6 @@ require (
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
@@ -47,6 +52,7 @@ require (
 	github.com/prometheus/common v0.44.0 // indirect
 	github.com/prometheus/procfs v0.10.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/testify v1.9.0
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.25.0 // indirect
 	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
--- internal/controller/cnfcertificationsuiterun_controller_test.go
+++ internal/controller/cnfcertificationsuiterun_controller_test.go
@@ -1,84 +1,244 @@
-/*
-Copyright 2024.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controller
 
 import (
 	"context"
+	"fmt"
+	"testing"
+	"time"
 
-	. "github.com/onsi/ginkgo/v2" //nolint:revive
-	. "github.com/onsi/gomega"    //nolint:revive
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
+	"github.com/stretchr/testify/assert"
 	cnfcertificationsv1alpha1 "github.com/test-network-function/cnf-certsuite-operator/api/v1alpha1"
+	"github.com/test-network-function/cnf-certsuite-operator/internal/controller/definitions"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
-var _ = Describe("CnfCertificationSuiteRun Controller", func() {
-	Context("When reconciling a resource", func() {
-		const resourceName = "test-resource"
+// Creates a reconciler with a fake client to mock API calls.
+func mockReconciler(objs []runtime.Object) *CnfCertificationSuiteRunReconciler {
+	s := scheme.Scheme
+	s.AddKnownTypes(cnfcertificationsv1alpha1.GroupVersion, &cnfcertificationsv1alpha1.CnfCertificationSuiteRun{})
 
-		ctx := context.Background()
+	runCR := &cnfcertificationsv1alpha1.CnfCertificationSuiteRun{}
+	cl := fake.NewClientBuilder().WithRuntimeObjects(objs...).WithStatusSubresource(runCR).Build()
 
-		typeNamespacedName := types.NamespacedName{
-			Name:      resourceName,
-			Namespace: "default", // TODO(user):Modify as needed
-		}
-		cnfcertificationsuiterun := &cnfcertificationsv1alpha1.CnfCertificationSuiteRun{}
+	return &CnfCertificationSuiteRunReconciler{Client: cl, Scheme: s}
+}
 
-		BeforeEach(func() {
-			By("creating the custom resource for the Kind CnfCertificationSuiteRun")
-			err := k8sClient.Get(ctx, typeNamespacedName, cnfcertificationsuiterun)
-			if err != nil && errors.IsNotFound(err) {
-				resource := &cnfcertificationsv1alpha1.CnfCertificationSuiteRun{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      resourceName,
-						Namespace: "default",
+func Test_getJobRunTimeThreshold(t *testing.T) {
+	tests := []struct {
+		name       string
+		timeoutStr string
+		want       time.Duration
+	}{
+		{ // Test case #1 - Pass with given timeout
+			name:       "Set timeout",
+			timeoutStr: "2h",
+			want:       2 * time.Hour,
+		},
+		{ // Test case #2 - Pass with default timeout as timeoutStr is an empty string
+			name:       "Empty timeout",
+			timeoutStr: "",
+			want:       time.Hour,
+		},
+	}
+
+	for _, tc := range tests {
+		assert.Equal(t, getJobRunTimeThreshold(tc.timeoutStr), tc.want)
+	}
+}
+
+func Test_getCertSuiteContainerExitStatus(t *testing.T) {
+	tests := []struct {
+		name           string
+		certSuitePod   *corev1.Pod
+		wantExitStatus int32
+		wantError      error
+	}{
+		{ // Test case #1 - Pass with returned exit status 0
+			name: "Container cnf-certsuite has 0 exit code",
+			certSuitePod: &corev1.Pod{
+				Status: corev1.PodStatus{
+					ContainerStatuses: []corev1.ContainerStatus{
+						{
+							Name: "cnf-certsuite",
+							State: corev1.ContainerState{
+								Terminated: &corev1.ContainerStateTerminated{
+									ExitCode: 0,
+								},
+							},
+						},
 					},
-					// TODO(user): Specify other spec details if needed.
+				},
+			},
+			wantExitStatus: 0,
+			wantError:      nil,
+		},
+		{ // Test case #2 - Pass with returned exit status -1
+			name: "Container cnf-certsuite has 0 exit code",
+			certSuitePod: &corev1.Pod{
+				Status: corev1.PodStatus{
+					ContainerStatuses: []corev1.ContainerStatus{
+						{
+							Name: "cnf-certsuite",
+							State: corev1.ContainerState{
+								Terminated: &corev1.ContainerStateTerminated{
+									ExitCode: -1,
+								},
+							},
+						},
+					},
+				},
+			},
+			wantExitStatus: -1,
+			wantError:      nil,
+		},
+		{ // Test case #3 - Fail with "container not found" error
+			name: "Container cnf-certsuite wasn't find in pod",
+			certSuitePod: &corev1.Pod{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "cnf-job-sample",
+					Namespace: "cnf-certsuite-operator",
+				},
+				Status: corev1.PodStatus{},
+			},
+			wantExitStatus: 0,
+			wantError:      fmt.Errorf("failed to get cert suite exit status: container not found in pod cnf-job-sample (ns cnf-certsuite-operator)"),
+		},
+	}
+	for _, tc := range tests {
+		gotExitStatus, gotErr := getCertSuiteContainerExitStatus(tc.certSuitePod)
+		assert.Equal(t, gotErr, tc.wantError)
+		if gotErr == nil { // no need to check exit status if an error has occurred
+			assert.Equal(t, gotExitStatus, tc.wantExitStatus)
+		}
+	}
+}
+
+func TestCnfCertificationSuiteRunReconciler_waitForCertSuitePodToComplete(t *testing.T) {
+	tests := []struct {
+		name               string
+		timeOut            time.Duration
+		phase              corev1.PodPhase
+		wantExitStatusCode int32
+		wantError          error
+	}{
+		{ // Test case #1 - Pass with exit status 0
+			name:               "Pass with Pod succeed phase",
+			timeOut:            time.Hour,
+			phase:              corev1.PodSucceeded,
+			wantExitStatusCode: 0,
+			wantError:          nil,
+		},
+		{ // Test case #2 - Fail, Pod stuck in running phase
+			name:               "Failed with Pod running phase",
+			timeOut:            10 * time.Second,
+			phase:              corev1.PodRunning,
+			wantExitStatusCode: 0,
+			wantError:          fmt.Errorf("timeout (10s) reached while waiting for cert suite pod cnf-certsuite-operator/cnf-job-sample to finish"),
+		},
+	}
+
+	certSuitePodNamespacedName := types.NamespacedName{
+		Name:      "cnf-job-sample",
+		Namespace: "cnf-certsuite-operator",
+	}
+	for _, tc := range tests {
+		certSuitePod := &corev1.Pod{
+			ObjectMeta: v1.ObjectMeta{
+				Name:      certSuitePodNamespacedName.Name,
+				Namespace: certSuitePodNamespacedName.Namespace,
+			},
+			Status: corev1.PodStatus{
+				Phase: tc.phase,
+			},
+		}
+
+		r := mockReconciler([]runtime.Object{certSuitePod})
+
+		gotExitStatusCode, gotError := r.waitForCertSuitePodToComplete(certSuitePodNamespacedName, tc.timeOut)
+		assert.Equal(t, gotError, tc.wantError)
+		if gotError == nil { // no need to check exit status if an error has occurred
+			assert.Equal(t, gotExitStatusCode, tc.wantExitStatusCode)
+		}
+	}
+}
+
+func TestCnfCertificationSuiteRunReconciler_updateStatus(t *testing.T) {
+	tests := []struct {
+		name                string
+		statusSetterFn      func(currStatus *cnfcertificationsv1alpha1.CnfCertificationSuiteRunStatus)
+		statusCheckerFn     func(currStatus *cnfcertificationsv1alpha1.CnfCertificationSuiteRunStatus) error
+		runCRNamespacedName types.NamespacedName
+		wantErr             bool
+	}{
+		{ // Test case #1 - Pass with exit status 0
+			name: "Pass when updating phase",
+			statusSetterFn: func(currStatus *cnfcertificationsv1alpha1.CnfCertificationSuiteRunStatus) {
+				currStatus.Phase = definitions.CnfCertificationSuiteRunStatusPhaseJobFinished
+			},
+			statusCheckerFn: func(currStatus *cnfcertificationsv1alpha1.CnfCertificationSuiteRunStatus) error {
+				if currStatus.Phase != definitions.CnfCertificationSuiteRunStatusPhaseJobFinished {
+					return fmt.Errorf("CnfCertificationSuiteRun status updated has failed. current status: %v, wanted status: %v",
+						currStatus.Phase, definitions.CnfCertificationSuiteRunStatusPhaseJobFinished)
 				}
-				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+				return nil
+			},
+			runCRNamespacedName: types.NamespacedName{
+				Name:      "cnf-run-sample",
+				Namespace: "cnf-certsuite-operator",
+			},
+			wantErr: false,
+		},
+		{ // Test case #1 - Fail, error = cnfcertificationsuiteruns.cnf-certifications.redhat.com "" not found
+			name: "Fail updating phase",
+			statusSetterFn: func(currStatus *cnfcertificationsv1alpha1.CnfCertificationSuiteRunStatus) {
+				currStatus.Phase = definitions.CnfCertificationSuiteRunStatusPhaseJobFinished
+			},
+			statusCheckerFn: func(currStatus *cnfcertificationsv1alpha1.CnfCertificationSuiteRunStatus) error {
+				if currStatus.Phase != definitions.CnfCertificationSuiteRunStatusPhaseJobFinished {
+					return fmt.Errorf("CnfCertificationSuiteRun status updated has failed. current status: %v, wanted status: %v",
+						currStatus.Phase, definitions.CnfCertificationSuiteRunStatusPhaseJobFinished)
+				}
+				return nil
+			},
+			runCRNamespacedName: types.NamespacedName{},
+			wantErr:             true,
+		},
+	}
+
+	runCR := &cnfcertificationsv1alpha1.CnfCertificationSuiteRun{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      "cnf-run-sample",
+			Namespace: "cnf-certsuite-operator",
+		},
+		Status: cnfcertificationsv1alpha1.CnfCertificationSuiteRunStatus{
+			Phase: definitions.CnfCertificationSuiteRunStatusCertSuiteRunning,
+		},
+	}
+	for _, tc := range tests {
+		r := mockReconciler([]runtime.Object{runCR})
+
+		// check whether an error has occurred if expected, and hasn't occurred if not expected
+		err := r.updateStatus(tc.runCRNamespacedName, tc.statusSetterFn)
+		if (err != nil) != tc.wantErr {
+			t.Errorf("CnfCertificationSuiteRunReconciler.updateStatus() error = %v, wantErr %v", err, tc.wantErr)
+		}
+
+		// check if status was updated (if an error hasn't occurred)
+		if err == nil {
+			updatedRunCR := cnfcertificationsv1alpha1.CnfCertificationSuiteRun{}
+			err := r.Get(context.TODO(), tc.runCRNamespacedName, &updatedRunCR)
+			if err != nil {
+				t.Errorf("Error getting updated Run CR ")
 			}
-		})
-
-		AfterEach(func() {
-			// TODO(user): Cleanup logic after each test, like removing the resource instance.
-			resource := &cnfcertificationsv1alpha1.CnfCertificationSuiteRun{}
-			err := k8sClient.Get(ctx, typeNamespacedName, resource)
-			Expect(err).NotTo(HaveOccurred())
-
-			By("Cleanup the specific resource instance CnfCertificationSuiteRun")
-			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
-		})
-		It("should successfully reconcile the resource", func() {
-			By("Reconciling the created resource")
-			controllerReconciler := &CnfCertificationSuiteRunReconciler{
-				Client: k8sClient,
-				Scheme: k8sClient.Scheme(),
+			err = tc.statusCheckerFn(&updatedRunCR.Status)
+			if err != nil {
+				t.Errorf(err.Error())
 			}
-
-			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
-				NamespacedName: typeNamespacedName,
-			})
-			Expect(err).NotTo(HaveOccurred())
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
-		})
-	})
-})
+		}
+	}
+}
